3a8821e1473be16e1e7e7bb4a703827a13a0088c
Leanna Hue <leanna.hue@gmail.com>
2020-01-08 16:03:13 -0600

    Slice 4: add code for fetching data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 
@@ -29,16 +29,16 @@ const schedule = {
 };
 
 const Banner = ({ title }) => (
-  <Title>{ title }</Title>
+  <Title>{title || '[loading...]'}</Title>
 );
 
 const CourseList = ({ courses }) => (
   <Button.Group>
-    { courses.map(course => <Course course={ course } />) }
+    {courses.map(course => <Course course={course} />)}
   </Button.Group>
 );
 
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
 
 const getCourseTerm = course => (
   terms[course.id.charAt(0)]
@@ -47,18 +47,33 @@ const getCourseTerm = course => (
 const getCourseNumber = course => (
   course.id.slice(1, 4)
 )
-  
+
 const Course = ({ course }) => (
   <Button>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+    {getCourseTerm(course)} CS {getCourseNumber(course)}: {course.title}
   </Button>
 );
 
-const App = () =>  (
-  <Container>
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </Container>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
+  return (
+    <Container>
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </Container>
+  );
+};
 
 export default App;
