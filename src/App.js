bb2813566741247b2416f8f01259a4946e1a50fe
Leanna Hue <leanna.hue@gmail.com>
2020-01-08 17:41:05 -0600

    Slice 6: add Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfGS00xA3xkyqeO7mL5Q3BJ0GppLPiOkA",
+  authDomain: "quick-react-556c4.firebaseapp.com",
+  databaseURL: "https://quick-react-556c4.firebaseio.com",
+  projectId: "quick-react-556c4",
+  storageBucket: "quick-react-556c4.appspot.com",
+  messagingSenderId: "480352769279",
+  appId: "1:480352769279:web:c5ff51a2686d940a6468ca",
+  measurementId: "G-5DPXQZH6C9",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
 
 const Banner = ({ title }) => (
   <Title>{title || '[loading...]'}</Title>
@@ -15,21 +31,21 @@ const useSelection = () => {
   const toggle = (x) => {
     setSelected(selected.includes(x) ? selected.filter(y => y !== x) : [x].concat(selected))
   };
-  return [ selected, toggle ];
+  return [selected, toggle];
 };
 
 const TermSelector = ({ state }) => (
   <Button.Group hasAddons>
-  { Object.values(terms)
-      .map(value => 
+    {Object.values(terms)
+      .map(value =>
         <Button key={value}
-          color={ buttonColor(value === state.term) }
-          onClick={ () => state.setTerm(value) }
-          >
-          { value }
+          color={buttonColor(value === state.term)}
+          onClick={() => state.setTerm(value)}
+        >
+          {value}
         </Button>
       )
-  }
+    }
   </Button.Group>
 );
 
@@ -37,14 +53,14 @@ const CourseList = ({ courses }) => {
   const [term, setTerm] = useState('Fall');
   const [selected, toggle] = useSelection();
   const termCourses = courses.filter(course => term === getCourseTerm(course));
- 
+
   return (
     <React.Fragment>
-      <TermSelector state={ { term, setTerm } } />
+      <TermSelector state={{ term, setTerm }} />
       <Button.Group>
-        { termCourses.map(course =>
-           <Course key={ course.id } course={ course }
-             state={ { selected, toggle } } />) }
+        {termCourses.map(course =>
+          <Course key={course.id} course={course}
+            state={{ selected, toggle }} />)}
       </Button.Group>
     </React.Fragment>
   );
@@ -61,7 +77,7 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 )
 
-const daysOverlap = (days1, days2) => ( 
+const daysOverlap = (days1, days2) => (
   days.some(day => days1.includes(day) && days2.includes(day))
 );
 
@@ -103,31 +119,42 @@ const addCourseTimes = course => ({
 
 const addScheduleTimes = schedule => ({
   title: schedule.title,
-  courses: schedule.courses.map(addCourseTimes)
+  courses: Object.values(schedule.courses).map(addCourseTimes)
 });
 
+const moveCourse = course => {
+  const meets = prompt('Enter new meeting data, in this format:', course.meets);
+  if (!meets) return;
+  const {days} = timeParts(meets);
+  if (days) saveCourse(course, meets); 
+  else moveCourse(course);
+};
+
+const saveCourse = (course, meets) => {
+  db.child('courses').child(course.id).update({meets})
+    .catch(error => alert(error));
+};
+
 const Course = ({ course, state }) => (
-  <Button color={ buttonColor(state.selected.includes(course)) }
-    onClick={ () => state.toggle(course) }
-    disabled={ hasConflict(course, state.selected) }
-    >
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <Button color={buttonColor(state.selected.includes(course))}
+    onClick={() => state.toggle(course)}
+    onDoubleClick={() => moveCourse(course)}
+    disabled={hasConflict(course, state.selected)}
+  >
+    {getCourseTerm(course)} CS {getCourseNumber(course)}: {course.title}
   </Button>
 );
 
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
   useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(addScheduleTimes(json));
+    const handleData = snap => {
+      if (snap.val()) setSchedule(addScheduleTimes(snap.val()));
     }
-    fetchSchedule();
-  }, [])
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+  }, []);
 
   return (
     <Container>
